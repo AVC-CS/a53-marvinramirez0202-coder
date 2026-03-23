4e5b08b69c3461003817dcfa95f2e0ddcfff1f90
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-23 16:32:49 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,17 @@ int main()
     int i, begin, end, sum = 0;
 
     // TODO: prompt user for begin and end values
+    cin >> begin >> end;
     // TODO: loop from begin to end and accumulate sum of even values
+    if (begin % 2 == 1 || (begin * -1) % 2 == 1){
+        begin = begin + 1;
+    }
+    i = begin;
+    while (i <= end){
+       sum += i;
+        i += 2;
+    }
+
+    cout << "The sum of the values is " << sum << endl;
+
 }
